52e6797f9b78aec020fe7fe6cf52538b34ee1e74
Hushanpreet77 <hushanpreetkaur86@gmail.com>
2025-03-18 11:22:44 -0600

    update

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ app.get('/home', (req, res) =>{
     res.send("Hello, World!");
 });
 
+//route handler for a post request using path parameter
+app.post('/user/:id',(req, res)=>{
+    const user_id = req.params.id;
+    res.send('This is a user id: ${user_id}')
+});
+
 //Start our server, listening on port 3000
 app.listen(PORT, () =>{
     console.log(`Connected on port ${PORT}`);
